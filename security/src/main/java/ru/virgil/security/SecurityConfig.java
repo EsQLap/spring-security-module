e42fe08aa654bfb3a8b843cb964038f6b73d2a08
EsQLap <61599652+EsQLap@users.noreply.github.com>
2023-05-10 16:40:11 +0400

    Добавлена настройка cors

--- security/src/main/java/ru/virgil/security/SecurityConfig.java
+++ security/src/main/java/ru/virgil/security/SecurityConfig.java
@@ -1,14 +1,22 @@
 package ru.virgil.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.virgil.security.entity.AuthMethods;
 import ru.virgil.security.firebase.FirebaseAuthenticationFilter;
 import ru.virgil.security.firebase.FirebaseAuthorizationProvider;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -33,6 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 // todo: стандартный редирект на страницу успешной безопасности
                 // todo: разобраться, как включить
+                .cors()
+                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .mvcMatchers(AUTH_PAGE_PATHS.values().toArray(String[]::new)).permitAll()
@@ -47,5 +57,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // .exceptionHandling()
         // .authenticationEntryPoint(authenticationEntryPoint)
         ;
+    }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration authCorsConfiguration = buildAuthCorsConfiguration();
+        CorsConfiguration resultCorsConfiguration = buildResultCorsConfiguration();
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration(AUTH_API_PATHS.get(AuthMethods.FIREBASE), authCorsConfiguration);
+        configurationSource.registerCorsConfiguration("/**", resultCorsConfiguration);
+        return configurationSource;
+    }
+
+    private CorsConfiguration buildResultCorsConfiguration() {
+        CorsConfiguration resultCorsConfiguration = new CorsConfiguration();
+        resultCorsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        resultCorsConfiguration.setAllowedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.COOKIE));
+        resultCorsConfiguration.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(Enum::name).toList());
+        resultCorsConfiguration.setAllowCredentials(true);
+        return resultCorsConfiguration;
+    }
+
+    private CorsConfiguration buildAuthCorsConfiguration() {
+        CorsConfiguration authCorsConfiguration = new CorsConfiguration();
+        authCorsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        authCorsConfiguration.setAllowedMethods(List.of(HttpMethod.POST.name()));
+        authCorsConfiguration.setAllowedHeaders(List.of(HttpHeaders.AUTHORIZATION));
+        authCorsConfiguration.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION));
+        authCorsConfiguration.setAllowCredentials(true);
+        return authCorsConfiguration;
     }
 }
